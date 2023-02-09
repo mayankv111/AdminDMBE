dbfc81c752ec4d110c2416445000fbb72adacedb
mayank virole <mayank@nextsolutions.in>
2023-02-09 10:31:15 +0530

    update  script plot controller

--- plotController.js
+++ plotController.js
@@ -9,13 +9,15 @@ const model=client.db("model");
 const collection1=model.collection("properties");
 const collection2 = model.collection("authority");
 // const collection3 = model.collection("temp_missingProperties");
+let c = 0;
 const rows=[]
-async function findAndUpdateProperty(plotNo , sectorNo , oname, mobile){
-	let res = await collection1.findOne({ plotNumber : plotNo , sectorNumber : sectorNo});
-	if(res === null) {
+async function findAndUpdateProperty(plotNo , sectorNo , oname, mobile, category){
+	let res = await collection1.findOne({ plotNumber : plotNo , sectorNumber : sectorNo , category : "PLOT"});
+	if(res == null) {
 		let resmiss = await collection1.insertOne({
 			plotNumber : plotNo , 
 			sectorNumber : sectorNo,
+			category : "PLOT"
 		})
 		console.log(resmiss);
 		await updatePropertyAuth(resmiss._id , oname, mobile);
@@ -27,8 +29,11 @@ async function findAndUpdateProperty(plotNo , sectorNo , oname, mobile){
 
 async function updatePropertyAuth(propid, oname, mobile) {
 	let res = await collection2.findOne({propertyID: ObjectId(propid)});
+	c++;
+	console.log(c);
 	if(res === null)
 	{
+		console.log("at not fond", mobile);
 		let res1 =await collection2.insertOne({
 			ownerName : oname , 
 			owenerFather : null,
@@ -39,16 +44,18 @@ async function updatePropertyAuth(propid, oname, mobile) {
 		console.log(res1);
 	}
 	else {
-		if(res.ownerName !== "" || res.phoneNumber !== "")
-		{
-			await collection2.updateOne(
-			{propertyID: ObjectId(propid)},
-			{ $set :{
-				ownerName : oname , 
-				phoneNumber : mobile ,  
-				}
-			});
-		}
+		console.log(res.ownerName);
+		// if(res.ownerName !== "" || res.phoneNumber !== "")
+		// {
+		// 	let res2 = await collection2.updateOne(
+		// 	{propertyID: ObjectId(propid)},
+		// 	{ $set :{
+		// 		ownerName : oname , 
+		// 		phoneNumber : mobile ,  
+		// 		}
+		// 	});
+		// 	console.log(res2);
+		// }
 	}
 }
 
@@ -57,7 +64,7 @@ async function test(filename) {
 		rows.push(row)
 	}).on("end",async function() {
 		for(let i=0;i<rows.length;i++) {
-			await findAndUpdateProperty(rows[i].Plot_Number, rows[i].Sector,rows[i].OWNER_NAME,rows[i].MOBILE);
+			await findAndUpdateProperty(rows[i].Plot_Number, rows[i].Sector,rows[i].OWNER_NAME,rows[i].MOBILE,rows[i].Category);
 		}
 		process.exit()
 	}).on("error",(error) => console.error(error))
@@ -67,3 +74,4 @@ exports.updateProp = (req,res) =>{
 	test(req.file.filename);
 	res.json({ "success" : "data updated"});
 }
+
