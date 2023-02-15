5d60b984009cd68d8051e4ae435b1ab5b9b06c9b
mayank virole <mayank@nextsolutions.in>
2023-02-15 14:36:28 +0530

    modified script for manual data upload

--- plotController.js
+++ plotController.js
@@ -7,55 +7,114 @@ const client=new MongoClient(process.env.P247_URI);
 const model=client.db("model");
 
 const collection1=model.collection("properties");
-const collection2 = model.collection("authority");
-// const collection3 = model.collection("temp_missingProperties");
-let c = 0;
+const collection2=model.collection("authority");
+
+let c=0;
 const rows=[]
-async function findAndUpdateProperty(plotNo , sectorNo , oname, mobile, category){
-	let res = await collection1.findOne({ plotNumber : plotNo , sectorNumber : sectorNo , category : "PLOT"});
-	if(res == null) {
-		let resmiss = await collection1.insertOne({
-			plotNumber : plotNo , 
-			sectorNumber : sectorNo,
-			category : "PLOT"
-		})
-		console.log(resmiss);
-		await updatePropertyAuth(resmiss._id , oname, mobile);
+async function findAndUpdateProperty(plotNo,sectorNo,oname,mobile) {
+	let res = await collection1.findOne({});
+	let res0=await collection1.findOne({
+		category: "PLOT",
+		plotNumber: plotNo.toUpperCase(),
+		sectorNumber: sectorNo.toUpperCase()
+	});
+	if(sectorNo==="South City 1") {
+		res=await collection1.findOne({plotNumber: plotNo,sectorNumber: "SC-1",category: "PLOT"});
 	}
-	else{
-	await updatePropertyAuth(res._id , oname, mobile);
+	else if(sectorNo==="South City 2") {
+		res=await collection1.findOne({plotNumber: plotNo,sectorNumber: "SC-2",category: "PLOT"});
+	}
+	else {
+		res=await collection1.findOne({plotNumber: plotNo,sectorNumber: sectorNo,category: "PLOT"});
+	}
+	console.log("res : ",res,"res0 : " ,res0);
+	if(res==null && res0 == null) {
+		let res1=await collection1.insertOne({
+			category: "PLOT",
+			plotNumber: plotNo.toUpperCase(),
+			sectorNumber: sectorNo.toUpperCase()
+		})
+		// console.log(res1);
+		await updatePropertyAuth(res1.insertedId,oname,mobile);
+	}
+
+	if(res && res0 === null){
+		let resupdate=await collection1.updateOne({
+			_id : ObjectId(res._id)
+		},{
+			$set: {
+				plotNumber: plotNo.toUpperCase(),
+				sectorNumber: sectorNo.toUpperCase(),
+			}
+		})
+		await updatePropertyAuth(res._id,oname,mobile);
+	}
+
+	if(res0 && res == null){
+		// console.log(1);
+		await updatePropertyAuth(res0._id,oname,mobile);
+	}
+
+	if(res && res0)
+	{
+		await collection1.updateOne({
+			_id : ObjectId(res._id)
+		},{
+			$set: {
+				plotNumber: plotNo.toUpperCase(),
+				sectorNumber: sectorNo.toUpperCase(),
+			}
+		})
+		await updatePropertyAuth(res._id, oname, mobile);
 	}
 }
 
-async function updatePropertyAuth(propid, oname, mobile) {
-	let res = await collection2.findOne({propertyID: ObjectId(propid)});
+async function updatePropertyAuth(propid,oname,mobile) {
+	let res=await collection2.findOne({propertyID: ObjectId(propid)});
+	// console.log(res, oname, mobile);
 	c++;
 	console.log(c);
-	if(res === null)
-	{
-		console.log("at not fond", mobile);
-		let res1 =await collection2.insertOne({
-			ownerName : oname , 
-			owenerFather : null,
-			address : "",
-			phoneNumber : mobile , 
-			propertyID: ObjectId(propid), 
-			});
-		console.log(res1);
+	if(res==null) {
+		await collection2.insertOne({
+			ownerName: oname,
+			owenerFather: "",
+			address: "",
+			phoneNumber: mobile,
+			propertyID: ObjectId(propid),
+		});
 	}
 	else {
-		console.log(res.ownerName);
-		// if(res.ownerName !== "" || res.phoneNumber !== "")
-		// {
-		// 	let res2 = await collection2.updateOne(
-		// 	{propertyID: ObjectId(propid)},
-		// 	{ $set :{
-		// 		ownerName : oname , 
-		// 		phoneNumber : mobile ,  
-		// 		}
-		// 	});
-		// 	console.log(res2);
-		// }
+		if(oname && mobile){
+			await collection2.updateOne(
+			{ _id : ObjectId(res._id) , },
+			{
+				$set: {
+					ownerName: oname,
+					phoneNumber: mobile,
+				}
+			});
+		}
+		else if(oname && !mobile)
+		{
+			await collection2.updateOne(
+				{ _id : ObjectId(res._id) },
+				{
+					$set: {
+						ownerName: oname,
+					}
+				});
+		}
+		else if(!oname && mobile)
+		{
+			await collection2.updateOne(
+				{ _id : ObjectId(res._id) },
+				{
+					$set: {
+						phoneNumber : mobile,
+					}
+				});
+		}
+
 	}
 }
 
@@ -63,15 +122,16 @@ async function test(filename) {
 	fs.createReadStream(`./uploads/${filename}`).pipe(csv()).on("data",async function(row) {
 		rows.push(row)
 	}).on("end",async function() {
-		for(let i=0;i<rows.length;i++) {
-			await findAndUpdateProperty(rows[i].Plot_Number, rows[i].Sector,rows[i].OWNER_NAME,rows[i].MOBILE,rows[i].Category);
+		for(let i=0;i< rows.length;i++) {
+			await findAndUpdateProperty(rows[i].Plot_Number,rows[i].Sector,rows[i].OWNER_NAME,rows[i].MOBILE);
 		}
 		process.exit()
 	}).on("error",(error) => console.error(error))
 }
 
-exports.updateProp = (req,res) =>{
-	test(req.file.filename);
-	res.json({ "success" : "data updated"});
-}
+// exports.updateProp = (req,res) =>{
+// 	test(req.file.filename);
+// 	res.json({ "success" : "data updated"});
+// }
 
+test("auth_update.csv");
